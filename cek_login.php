3d7c7a74d6d5d72c0c73f5fcd0afe20ad1858e98
irsyadali1 <49668571+irsyadali1@users.noreply.github.com>
2019-10-16 14:39:54 +0700

    Add files via upload
    ini login & register system

---
+++ cek_login.php
@@ -0,0 +1,31 @@
+<?php 
+
+echo session_save_path();
+session_start();
+
+// menghubungkan php dengan koneksi database
+include 'koneksi.php';
+ 
+// menangkap data yang dikirim dari form login
+$username = $_POST['username'];
+$password = $_POST['password'];
+ 
+echo "$username $password";
+ 
+// menyeleksi data user dengan username dan password yang sesuai
+$login = mysqli_query($koneksi,"select * from login where username='$username' and password='$password'");
+// menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($login);
+header();
+ 
+// cek apakah username dan password di temukan pada database
+if($cek > 0){	
+ 		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['password'] = $password; 
+		// alihkan ke halaman dashboard admin
+		header("location:index.html");
+}else{
+	header("location:index.php?pesan=gagal");
+}
+?>
---
+++ register.php
@@ -0,0 +1,30 @@
+<?php
+
+    require_once("koneksi.php");
+
+    if(isset($_POST['register'])){
+    
+    include 'koneksi.php';
+    $name= mysqli_real_escape_string($koneksi, trim($_POST['name']));
+    $username= mysqli_real_escape_string($koneksi, trim($_POST['username']));
+    $password= mysqli_real_escape_string($koneksi, trim($_POST['pass']));
+    $email= mysqli_real_escape_string($koneksi, trim($_POST['email']));
+    // filter data yang diinputkan
+    /*$name = filter_input(INPUT_POST, 'name'?/*, FILTER_SANITIZE_STRING);
+    $username = filter_input(INPUT_POST, 'username'*, FILTER_SANITIZE_STRING);
+    // enkripsi password
+    $password = password_hash($_POST["pass"], PASSWORD_DEFAULT);
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);*/
+
+
+    // menyiapkan query
+    $sql = "insert into login (username,password, email, fullname) 
+            values ('$username','$password','$email','$name' )";
+    
+    // eksekusi query untuk menyimpan ke database
+    mysqli_query($koneksi, $sql);
+    header("Location: lgn/login.html");
+
+
+}
+?>
